ac25635176f4c960e1d8c905e7df39e154f45ba2
Piyush <psantwani@gmail.com>
2017-10-11 01:36:45 +0530

    redux-thunk example

---
+++ src/components/app.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class App extends Component {
+  
+  componentWillMount(){
+    this.props.fetchUsers();
+  }
+  
+  renderUser({id,name,email}) {
+    return (
+      <li className="list-group-item" key={id}>
+        <span className="label label-default label-pill pull-xs-right">
+          <a href={email}>{email}</a>
+        </span>
+        {name}
+      </li>
+    );
+  }
+
+  render(){
+    return(
+      <div>
+        <h4>Email directory</h4>
+        <ul className="list-group">
+          {this.props.users.map(this.renderUser)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default connect((state => state), actions)(App);
